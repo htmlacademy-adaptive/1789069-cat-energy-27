da05512b7ea47754260ca73d0249235672ce52ec
User <01txxt10@gmail.com>
2023-01-07 19:00:55 +0700

    Добавление поддержки формата webp для контентных изображений

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ import terser from 'gulp-terser';
 import squoosh from 'gulp-libsquoosh';
 import svgo from 'gulp-svgmin';
 import svgstore from 'gulp-svgstore';
-import { deleteAsync } from 'del';
+import del from 'del';
 
 // Styles
 export const styles = () => {
@@ -93,8 +93,8 @@ const copy = (done) => {
 }
 
 // Clean
-export const clean = () => {
-  return deleteAsync('build');
+const clean = () => {
+  return del('build');
 }
 
 // Server
